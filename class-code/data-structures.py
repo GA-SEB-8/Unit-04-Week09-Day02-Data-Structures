8b2c915fd422cb5dcdc7cc95341bab7a4118b46d
omarakamal <omaraakamal@gmail.com>
2025-08-25 11:29:49 +0300

    update

---
+++ class-code/data-structures.py
@@ -0,0 +1,18 @@
+# Lists
+# Dictionaries
+# Tupples
+
+my_students = ['Abdullah','Malak','Amna','Mohammad','Ali']
+
+
+
+
+# access 1 value in my List
+print(my_students[2])
+
+
+# Changing value in array
+my_students[1] = 'Yaqoob'
+
+
+print(my_students)
---
+++ class-code/index.js
@@ -0,0 +1,2 @@
+let my_students = ['Abdullah','Malak','Amna','Mohammad','Ali']
+console.log(my_students[0])
