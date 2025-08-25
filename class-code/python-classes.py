24972d3b70993085f2ccc70f0bf2fe7a1d7f1ac8
omarakamal <omaraakamal@gmail.com>
2025-08-25 15:45:46 +0300

    update

---
+++ class-code/python-classes.py
@@ -0,0 +1,21 @@
+cat1 = {
+    'name':'zoro',
+    'age':2
+}
+
+cat2 = {
+    'name':'luffy',
+    'age':7
+}
+
+
+# creating a new class
+class Cat():
+    def __init__():
+        
+
+
+# creating a new object instance of a cat class
+cat3 = Cat('luffy',10)
+
+print(cat3)
