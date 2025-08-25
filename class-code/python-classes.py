38693a29069dc82747f3a93bea0e085322aa76fd
omarakamal <omaraakamal@gmail.com>
2025-08-25 16:25:58 +0300

    update

--- class-code/python-classes.py
+++ class-code/python-classes.py
@@ -28,6 +28,7 @@ class Cat():
 
 
 
+
 # creating a new object instance of a cat class
 cat3 = Cat('luffy',10, 'red')
 cat4 = Cat('zoro',4)
@@ -38,3 +39,31 @@ cat3.speak()
 
 
 print(cat3.name)
+
+
+
+# exercise 1: create a class called Student
+# this class should have the following properties: name and country
+# this class should have 1 method introduce_self(): which should print
+# my name is {name} and I am from {country}
+
+class Student():
+
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+    def introduce_self(self):
+        print(f"my name is {self.name} and I am from {self.country}")
+
+
+
+mohammad = Student('Mohammad','Bahrian')
+yusef = Student('yusef','Bahrian')
+
+
+
+# changing the value of a property for the object
+mohammad.country='Saudi'
+
+print(mohammad.country)
